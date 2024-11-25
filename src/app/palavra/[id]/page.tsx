9e4a5ca828da6a28870cbe6eda74bbaa2ed27bb0
Pedro Barreto <pedro.barreto.ne@gmail.com>
2024-11-24 23:06:45 -0300

    att:final

--- src/app/palavra/[id]/page.tsx
+++ src/app/palavra/[id]/page.tsx
@@ -30,6 +30,7 @@ export default function GamePage({ params }: GamePageProps) {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
   const [showCongrats, setShowCongrats] = useState<boolean>(false);
+  const [showFinalCongrats, setShowFinalCongrats] = useState<boolean>(false);
 
   const handleKeyPress = (button: string) => {
     if (button === "{bksp}") {
@@ -67,14 +68,32 @@ export default function GamePage({ params }: GamePageProps) {
   };
 
   const handleNextPhase = () => {
-    router.push(`/palavra/${wordId + 1}`);
+    if (wordId === words[words.length - 1].id) {
+      // Se for o último jogo, exibe mensagem final
+      setShowCongrats(false);
+      setShowFinalCongrats(true);
+    } else {
+      router.push(`/palavra/${wordId + 1}`);
+    }
   };
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-6 bg-[url('/image.jpg')]">
       <div className="flex flex-col items-center justify-center bg-white rounded-lg p-6 py-16">
         {showConfetti && <Confetti />}
-        {showCongrats ? (
+        {showFinalCongrats ? (
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <h1 className="text-4xl font-bold text-green-600 text-center">
+              🎉 Parabéns! Você completou todos as palavras! 🎉
+            </h1>
+            <button
+              onClick={() => router.push('/')}
+              className="px-6 py-3 bg-[#222] text-white font-semibold rounded-md shadow hover:bg-[#000] transition-colors"
+            >
+              Voltar para a Página Inicial
+            </button>
+          </div>
+        ) : showCongrats ? (
           <div className="flex flex-col items-center justify-center space-y-4">
             <h1 className="text-4xl font-bold text-green-600 text-center">Parabéns! Você acertou!</h1>
             <button
